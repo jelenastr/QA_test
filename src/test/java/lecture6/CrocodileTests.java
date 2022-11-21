52a2fab0e57f329ce71527efcb0656a5ff90d8bc
JelenaS <jelena.strunevska@gmail.com>
2022-11-21 09:47:46 +0200

    Lesson CrocodileTests

--- src/test/java/lecture6/CrocodileTests.java
+++ src/test/java/lecture6/CrocodileTests.java
@@ -1,34 +1,33 @@
 package lecture6;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-//import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
-//
-//public class CrocodileTests {
-//
-//    Crocodile crocodile = new Crocodile();
-//    boolean hungry = true;
-//
-//    @Test
-//    public void testForCrocodile() {
-//
-//        System.out.println(crocodile.getCrocodileWeight());
-//        System.out.println(crocodile.getCrocodileColor());
-//        System.out.println(crocodile.getCrocodileLength());
-//        System.out.println(crocodile.getCrocodileName());
-//        System.out.println(crocodile.getCrocodileHungry());
-//
-//        crocodile.setCrocodileWeight(1200);
-//        crocodile.setCrocodileColor("Dark green");
-//        crocodile.setCrocodileLength(7);
-//        crocodile.setCrocodileName("The saltwater crocodile");
-//        crocodile.setCrocodileHungry(hungry);
-//
-//        assertThat(crocodile.getCrocodileWeight()).isCloseTo(1200);
-//        assertThat(crocodile.getCrocodileColor()).isEqualTo("Dark green");
-//        assertThat(cr)
-//
-//    }
-//}
+
+import org.assertj.core.data.Percentage;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+public class CrocodileTests {
+
+    Crocodile crocodile = new Crocodile();
+    boolean hungry = true;
+
+
+    @Test
+    public void testForCrocodile() {
+
+        crocodile.setCrocodileWeight(1000);
+        crocodile.setCrocodileColor("Dark green");
+        crocodile.setCrocodileLength(7);
+        crocodile.setCrocodileName("The Nile crocodile");
+        crocodile.setCrocodileHungry(hungry);
+
+        assertThat(crocodile.getCrocodileWeight()).isCloseTo(1000, Percentage.withPercentage(15));
+        assertThat(crocodile.getCrocodileColor()).isEqualTo("Dark green");
+        assertThat(crocodile.getCrocodileLength()).isGreaterThan(6);
+        assertThat(crocodile.getCrocodileName()).isEqualTo("The saltwater crocodile");
+        assertThat(crocodile.getCrocodileHungry()).isEqualTo(hungry);
+    }
+}
 
