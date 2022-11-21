dc555e202e7412a713280347616324222cea74be
JelenaS <jelena.strunevska@gmail.com>
2022-11-21 09:17:22 +0200

    Lesson Movies

---
+++ src/test/java/lecture6/Crocodile.java
@@ -0,0 +1,52 @@
+package lecture6;
+
+public class Crocodile {
+
+
+    public int getCrocodileWeight() {
+        return crocodileWeight;
+    }
+
+    public void setCrocodileWeight(int crocodileWeight) {
+        this.crocodileWeight = crocodileWeight;
+    }
+
+    public String getCrocodileColor() {
+        return crocodileColor;
+    }
+
+    public void setCrocodileColor(String crocodileColor) {
+        this.crocodileColor = crocodileColor;
+    }
+
+    public int getCrocodileLength() {
+        return crocodileLength;
+    }
+
+    public void setCrocodileLength(int crocodileLength) {
+        this.crocodileLength = crocodileLength;
+    }
+
+    public String getCrocodileName() {
+        return crocodileName;
+    }
+
+    public void setCrocodileName(String crocodileName) {
+        this.crocodileName = crocodileName;
+    }
+
+    public Boolean getCrocodileHungry() {
+        return crocodileHungry;
+    }
+
+    public void setCrocodileHungry(Boolean crocodileHungry) {
+        this.crocodileHungry = crocodileHungry;
+    }
+
+    private int crocodileWeight;
+    private String crocodileColor;
+    private int crocodileLength;
+    private String crocodileName;
+    private Boolean crocodileHungry;
+
+}
---
+++ src/test/java/lecture6/CrocodileTests.java
@@ -0,0 +1,34 @@
+package lecture6;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+//
+//public class CrocodileTests {
+//
+//    Crocodile crocodile = new Crocodile();
+//    boolean hungry = true;
+//
+//    @Test
+//    public void testForCrocodile() {
+//
+//        System.out.println(crocodile.getCrocodileWeight());
+//        System.out.println(crocodile.getCrocodileColor());
+//        System.out.println(crocodile.getCrocodileLength());
+//        System.out.println(crocodile.getCrocodileName());
+//        System.out.println(crocodile.getCrocodileHungry());
+//
+//        crocodile.setCrocodileWeight(1200);
+//        crocodile.setCrocodileColor("Dark green");
+//        crocodile.setCrocodileLength(7);
+//        crocodile.setCrocodileName("The saltwater crocodile");
+//        crocodile.setCrocodileHungry(hungry);
+//
+//        assertThat(crocodile.getCrocodileWeight()).isCloseTo(1200);
+//        assertThat(crocodile.getCrocodileColor()).isEqualTo("Dark green");
+//        assertThat(cr)
+//
+//    }
+//}
+
