2e3b0bdd6fa50d64388130bf8e8da747c425927a
JelenaS <jelena.strunevska@gmail.com>
2022-12-01 23:46:22 +0200

    Lesson 7 CarSeleniumDemo AliExpressSeleniumDemo

---
+++ src/test/java/lecture7/SeleniumDemo.java
@@ -0,0 +1,31 @@
+package lecture7;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class SeleniumDemo {
+    @Test
+    public void someDemoTest() {
+        String path = "C:\\Users\\jelen\\IdeaProjects\\SomeProjectGradle\\src\\main\\resources\\chromedriver.exe";
+        System.setProperty("webDriver.chrome.driver", path);
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://www.ss.lv/");
+
+        driver.findElement(By.id("mtd_300")).click();
+
+        Select dealType = new Select(driver.findElement(By.cssSelector("select[name='sid']")));
+//        Select dealType = new Select(driver.findElement(By.xpath("select[name='sid']")));
+
+        dealType.selectByValue("/lv/animals/dogs/sell/");
+
+        driver.findElement(By.id("f_o_8_min")).sendKeys("20");
+        driver.findElement(By.id("f_o_1276_min")).sendKeys("2");
+
+        driver.findElement(By.xpath("//input[@class='b s12']")).click();
+
+        //        driver.quit();
+    }
+
+}
